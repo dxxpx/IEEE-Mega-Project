d7c537879906e423a863680b007a5815e5135c11
dxxpx <dxxpka.20@gmail.com>
2025-01-21 00:10:54 +0530

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,14 @@ import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import '../viewmodels/changes.dart';
 import '../views/user_online/register_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  String apikey = dotenv.env['OPENAI_API_KEY'] ?? "";
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyCA5JJe1J3okqpAVkvICIg3eQB_CvvkJ-M",
+    options: FirebaseOptions(
+      apiKey: apikey,
       appId: "1:795621005285:android:985e46eb36c7a2171f410c",
       messagingSenderId: "795621005285",
       projectId: "hackfest-d74d3",
